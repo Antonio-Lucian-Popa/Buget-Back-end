9f536b79ee34cfdb10dbb17af292e211e065488a
asu <antonio.popa.dev@gmail.com>
2025-11-28 18:08:44 +0200

    Switch to Postgres (Neon)

--- src/routes/debtRoutes.js
+++ src/routes/debtRoutes.js
@@ -1,13 +1,15 @@
+// src/routes/debtRoutes.js
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
-const { createDebt, getDebts, updateDebtStatus, deleteDebt } = require('../controllers/debtController');
+const debtController = require('../controllers/debtController');
 
 router.use(authMiddleware);
 
-router.post('/', createDebt);
-router.get('/', getDebts);
-router.patch('/:id/status', updateDebtStatus);
-router.delete('/:id', deleteDebt);
+// rute
+router.post('/', debtController.createDebt);
+router.get('/', debtController.getDebts);
+router.patch('/:id/status', debtController.updateDebtStatus);
+router.delete('/:id', debtController.deleteDebt);
 
 module.exports = router;
